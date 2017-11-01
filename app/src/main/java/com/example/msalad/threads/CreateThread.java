324144f692cceb9f77670a6a24f66d58ce072f18
mohamed <msalad@uncc.edu>
2017-11-01 11:37:21 -0400

    Added list details for localfragment

--- app/src/main/java/com/example/msalad/threads/CreateThread.java
+++ app/src/main/java/com/example/msalad/threads/CreateThread.java
@@ -1,12 +1,17 @@
 package com.example.msalad.threads;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -17,14 +22,19 @@ import com.google.firebase.database.FirebaseDatabase;
 public class CreateThread extends AppCompatActivity {
 
     private Button cThread;
-    private TextView tThread;
+    private EditText tThread;
     private DatabaseReference threadRef;
     private FirebaseDatabase database;
+    private String threadCode;
+    private String threadTitle;
+    private String MY_PREFS_NAME = "MY_PREFS_NAME";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_thread);
+
+
 
         cThread = findViewById(R.id.createThreadBtn);
         tThread = findViewById(R.id.threadTitle);
@@ -35,5 +45,35 @@ public class CreateThread extends AppCompatActivity {
             }
         });
 
+        tThread.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                threadTitle = s.toString();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseApp.initializeApp(this);
+        database = FirebaseDatabase.getInstance();
+        threadRef = database.getReference();
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        String restoredText = prefs.getString("text", null);
+        if (restoredText != null) {
+            threadCode = prefs.getString("threadCode", "8080");//"No name defined" is the default value.
+        }
     }
 }
