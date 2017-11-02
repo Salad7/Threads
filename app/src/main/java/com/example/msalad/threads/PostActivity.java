6a184b8629690b5a0305c8ca9d3d1d52bd38c532
mohamed <msalad@uncc.edu>
2017-11-02 01:13:59 -0400

    Added list details for localfragment

--- app/src/main/java/com/example/msalad/threads/PostActivity.java
+++ app/src/main/java/com/example/msalad/threads/PostActivity.java
@@ -1,15 +1,34 @@
 package com.example.msalad.threads;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by cci-loaner on 11/1/17.
@@ -27,7 +46,12 @@ public class PostActivity extends AppCompatActivity {
     TextView upvoteCount;
     Button upvoteBtn;
     Post incomingPost;
-
+    String threadCode;
+    EditText et_reply;
+    ImageButton send;
+    int topicPostion;
+    ArrayList<Message> messages;
+    int messagePosition;
 
 
     @Override
@@ -41,6 +65,8 @@ public class PostActivity extends AppCompatActivity {
         message = findViewById(R.id.post_msg);
         upvoteBtn = findViewById(R.id.upvoteBtn);
         upvoteCount = findViewById(R.id.post_upvotes);
+        send = findViewById(R.id.send);
+        messages = new ArrayList<>();
         if(getIntent().getExtras() != null){
             incomingPost = (Post) getIntent().getSerializableExtra("post");
             title.setText(getIntent().getStringExtra("tt"));
@@ -55,6 +81,76 @@ public class PostActivity extends AppCompatActivity {
             }
         }
 
+        et_reply = findViewById(R.id.reply_field);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(et_reply.getText().toString().length() > 0){
+                    DatabaseReference newMessagePath = threadRef.child("Threads").child(threadCode).child("topics").child(topicPostion+"").child("messages").child(messagePosition+"");
+                }
+            }
+        });
+
+    }
+
+    public void loadPosts(){
+        threadRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int currentLowest = -1;
+                messages.clear();
+                if(dataSnapshot.child("Threads").child(threadCode).exists()){
+                    DataSnapshot threadPath = dataSnapshot.child("Threads").child(threadCode);
+                    if(threadPath.child("topics").exists()){
+                        DataSnapshot topicPath = threadPath.child("topics");
+                        if(topicPath.child(topicPostion+"").child("messages").exists()){
+                            DataSnapshot messagesPath = topicPath.child(topicPostion+"").child("messages");
+                            int totalMessages = (int) messagesPath.getChildrenCount();
+                            int messagesFound = 0;
+                            for(int i = 0; i < 10000; i++){
+                                if(messagesPath.child(i+"").exists() && totalMessages >= messagesFound){
+                                DataSnapshot specificMessagesPath = messagesPath.child(i+"");
+                                if(specificMessagesPath.child("anonCode").exists()){
+                                Message message = new Message();
+                                    if(specificMessagesPath.child("upvoters").exists()){
+                                    message.setUpvoters((ArrayList)specificMessagesPath.child("upvoters").getValue());
+                                    }
+                                message.setMsg(specificMessagesPath.child("message").getValue(String.class));
+                                message.setPosition(i);
+                                message.setTimeStamp(specificMessagesPath.child("message").getValue(Integer.class));
+                                message.setAnonCode((Map) specificMessagesPath.child("message").getValue());
+                                message.setReplies(specificMessagesPath.child("replies").getValue(Integer.class));
+                                message.setUpvotes(specificMessagesPath.child("upvotes").getValue(Integer.class));
+                                messages.add(message);
+                                    messagesFound += 1;
+
+                                }
+                                }
+                                else{
+                                    if(currentLowest == -1){
+                                    currentLowest = i;
+                                        messagePosition = i;
+                                    }
+                                }
+
+                            }
+                        }
+                    }else{
+                        messagePosition = 0;
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 
     @Override
@@ -63,5 +159,63 @@ public class PostActivity extends AppCompatActivity {
         FirebaseApp.initializeApp(this);
         database = FirebaseDatabase.getInstance();
         threadRef = database.getReference();
+    }
+
+
+    public class MessageAdapter extends ArrayAdapter<Message> {
+        Context c;
+        int res;
+        List<Message> messages;
+        public MessageAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Message> objects) {
+            super(context, resource, objects);
+            c = context;
+            res = resource;
+            messages = objects;
+        }
+
+        @Override
+        public int getCount() {
+            return messages.size();
+        }
+
+        @NonNull
+        @Override
+        public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) c
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(res,parent,false);
+            TextView label;
+            TextView msg;
+            TextView count;
+            Button upvoteBtn;
+            label = convertView.findViewById(R.id.posttitle);
+            msg = convertView.findViewById(R.id.post_reply);
+            count = convertView.findViewById(R.id.post_upvote_count);
+            upvoteBtn = convertView.findViewById(R.id.upvote_img);
+            label.setText(position+"");
+            msg.setText(messages.get(position).getMsg());
+            count.setText(messages.get(position).getUpvoters().size()+"");
+            upvoteBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String androidId = Settings.Secure.getString(PostActivity.this.getContentResolver(),
+                            Settings.Secure.ANDROID_ID);
+                    if(messages.get(position).getUpvoters() == null || !messages.get(position).getUpvoters().contains(androidId)){
+                        ArrayList<String> androids = new ArrayList<String>();
+                        androids.add(androidId);
+                        HashMap map = new HashMap();
+                        map.put("upvoters",androids);
+                        threadRef.child("Threads").child(threadCode).child("topics").child(topicPostion+"").child("messages").child(messages.get(position).getPosition()+"")
+                                .updateChildren(map);
+                    }
+//                    else if(!messages.get(position).getUpvoters().contains(androidId)){
+//
+//                    }
+                }
+            });
+
+
+            return convertView;
+        }
     }
 }
